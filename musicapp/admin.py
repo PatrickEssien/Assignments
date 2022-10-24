1f370f9c297a4f39daf61144b603a1a71cdac8a0
PatrickEssien <81172875+PatrickEssien@users.noreply.github.com>
2022-10-24 16:17:43 +0100

    Add files via upload

---
+++ musicapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from.models import artiste, Songs
+# Register  your models here.
+# I registered the models and attributes i created here.
+admin.site.register(artiste)
+admin.site.register(Songs)
